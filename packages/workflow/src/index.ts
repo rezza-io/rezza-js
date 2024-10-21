8b8f437d1d347eb1d193c4d6d455014f9f65c656
Ethan Dang <tiencuong.dang@gmail.com>
2024-10-21 10:24:06 +0700

    chore: bump version to 0.3.1 and update TypeBox import
    - Increment version from 0.3.0 to 0.3.1 in package.json and jsr.json
    - Update TypeBox import to use more specific path for better tree-shaking

--- packages/workflow/src/index.ts
+++ packages/workflow/src/index.ts
@@ -1,7 +1,7 @@
 export * from "./types";
 export * from "./workflow";
 export * from "./workflow-builder";
-export { Type as t } from "@sinclair/typebox";
+export { Type as t } from "@sinclair/typebox/type";
 export { Parse as parse } from "@sinclair/typebox/value";
 
 export * as schemas from "./schemas";
